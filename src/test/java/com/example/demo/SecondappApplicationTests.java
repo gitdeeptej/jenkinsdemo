05e924675f08d9b7ad5197ab2da5b15ebf4d0772
gitdeeptej <gitdeeptej@gmail.com>
2022-08-04 20:00:07 +0530

    again

--- src/test/java/com/example/demo/SecondappApplicationTests.java
+++ src/test/java/com/example/demo/SecondappApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.controller.WelcomeController;
 
 @SpringBootTest
 class SecondappApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void welcome() {
+		WelcomeController wc=new WelcomeController();
+		String resp=wc.getMessage();
+		System.out.println(resp);
+		assertEquals("  elcome", resp);
 	}
 
 }
